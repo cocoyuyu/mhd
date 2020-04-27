c4693eb166067a2605652b83219452e86d7915c8
cocoyuyu <2521692430@qq.com>
2020-04-27 18:05:40 +0800

    '路由懒加载'

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,36 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import store from '../store'
 
-import Home from '../views/Home'
-import Classify from '../views/Classify'
-import Hello from '../views/Hello'
-import Favorite from '../views/Hello/Favorite'
-import History from '../views/Hello/History'
-import Login from '../views/Login'
-import Register from '../views/Register'
-import Ranking from '../views/Ranking'
-import My from '../views/My'
-import Search from '../views/Search'
-import SearchResult from '../views/SearchResult'
-import Vip from '../views/Vip'
-import City from '../views/City'
+// 同步引入路由页面组件
+// import Home from '../views/Home'
+// import Classify from '../views/Classify'
+// import Hello from '../views/Hello'
+// import Favorite from '../views/Hello/Favorite'
+// import History from '../views/Hello/History'
+// import Login from '../views/Login'
+// import Register from '../views/Register'
+// import Ranking from '../views/Ranking'
+// import My from '../views/My'
+// import Search from '../views/Search'
+// import SearchResult from '../views/SearchResult'
+// import Vip from '../views/Vip'
+// import City from '../views/City'
+
+// 异步引入路由页面组件(路由懒加载)
+// 改造：定义变量接收一个函数，把引入的文件返回
+// const Home = () => import('../views/Home')
+const Classify = () => import('../views/Classify')
+const Hello = () => import('../views/Hello')
+const Favorite = () => import('../views/Hello/Favorite')
+const History = () => import('../views/Hello/History')
+const Login = () => import('../views/Login')
+const Register = () => import('../views/Register')
+const Ranking = () => import('../views/Ranking')
+const My = () => import('../views/My')
+const Search = () => import('../views/Search')
+const SearchResult = () => import('../views/SearchResult')
+const Vip = () => import('../views/Vip')
+const City = () => import('../views/City')
 
 // 调用路由
 Vue.use(VueRouter)
@@ -23,7 +40,8 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/home',
-    component: Home
+    // /* webpackChunkName:"home" */ 给拆分出去的js文件取个名字
+    component: () => import(/* webpackChunkName:"home" */ '../views/Home')
   },
   {
     path: '/classify',
